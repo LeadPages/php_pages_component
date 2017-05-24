72b4e08252683424e371e1b646ddc919eb68b1d0
Tim Hart <tim.hart@ave81.com>
2017-05-24 11:29:24 -0500

    changed header to authorization

--- src/Pages/LeadpagesPages.php
+++ src/Pages/LeadpagesPages.php
@@ -57,7 +57,7 @@ class LeadpagesPages
         try {
             $response = $this->client->get($this->PagesUrl,
                 [
-                    'headers' => ['LP-Security-Token' => $this->login->token],
+                    'headers' => ['Authorization' => $this->login->token],
                     'verify' => $this->certFile,
                     'query' => $queryArray
                 ]);
@@ -195,7 +195,7 @@ class LeadpagesPages
         try {
             $response = $this->client->get($this->PagesUrl . '/' . $pageId,
                 [
-                    'headers' => ['LP-Security-Token' => $this->login->token],
+                    'headers' => ['Authorization' => $this->login->token],
                     'verify' => $this->certFile,
                 ]);
 
@@ -329,7 +329,7 @@ class LeadpagesPages
         try {
             $response = $this->client->get($this->PagesUrl . '/' . $pageId,
                 [
-                    'headers' => ['LP-Security-Token' => $this->login->token],
+                    'headers' => ['Authorization' => $this->login->token],
                     'verify' => $this->certFile,
                 ]);
 
